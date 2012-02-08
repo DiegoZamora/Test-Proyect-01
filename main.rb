7e219bb773bb23a1e8bff57eb342b38da80f9180
Diego Zamora <dzamora@redvel.cl>
2012-02-08 11:34:04 -0300

    getting ip address from request

--- main.rb
+++ main.rb
@@ -1,10 +1,5 @@
 require 'sinatra'
 require 'slim'
-get '/:task' do
-  @task = params[:task].split('-').join(' ').capitalize
-  slim :task
-end
-post '/' do
-  @task =  params[:task]
-  slim :task
+get '/index' do
+  slim :index
 end
